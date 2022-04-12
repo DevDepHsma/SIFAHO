b4b52179222d747cad796ff8c9cab6151c6f8be2
paul <paul.18.sma@gmail.com>
2022-04-12 08:47:06 -0300

    improve add_products def

--- spec/support/helpers/order.rb
+++ spec/support/helpers/order.rb
@@ -11,7 +11,7 @@ module Helpers
     # products => array of products (should be persisted)
     # products_size => number of products that sample will rake
     #  **fields => filds that should been fill it
-    def add_products(products, products_size, *fields_args)
+    def add_products(products, products_size, **fields_args)
       products.sample(products_size).each_with_index do |product, index|
         page.execute_script %Q{
           $($('input.product-code')[#{index}]).val("#{product[1]}").keydown()
@@ -19,9 +19,17 @@ module Helpers
         sleep 1
         expect(find('ul.ui-autocomplete')).to have_content(product[1].to_s)
         page.execute_script("$('.ui-menu-item:contains(#{product[1]})').first().click()")
-        page.execute_script %Q{
-          $($('input.request-quantity')[#{index}]).val(#{rand(100..750)}).keydown()
-        }
+        if fields_args.include?(:request_quantity)
+          page.execute_script %Q{
+            $($('input.request-quantity')[#{index}]).val(#{rand(100..750)}).keydown()
+          }
+        end
+        if fields_args.include?(:observations)
+          # 74 characters
+          page.execute_script %Q{
+            $($('textarea.observations')[#{index}]).val('Lorem Ipsum is simply dummy text of the printing and typesetting industry.').keydown()
+          }
+        end
         page.execute_script %Q{$('button.btn-save').first().click()}
         sleep 1
         click_link 'Agregar producto' unless (index + 1).eql?(products_size)
