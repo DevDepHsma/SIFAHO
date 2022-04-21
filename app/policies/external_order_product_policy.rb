f5a29fec628599e06e2d5b7a8fc1f79c21cfb18f
paul <paul.18.sma@gmail.com>
2022-04-21 14:39:57 -0300

    adds external order provider 'edit' / 'accept' rspec

--- app/policies/external_order_product_policy.rb
+++ app/policies/external_order_product_policy.rb
@@ -1,7 +1,7 @@
 class ExternalOrderProductPolicy < ApplicationPolicy
   def edit_request_quantity?
     if record.is_provision? && record.get_order.proveedor_auditoria?
-      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :central_farmaceutico, :medic, :enfermero)
+      user.has_permission?(:update_external_order_provider)
     elsif record.is_solicitud? && record.get_order.solicitud_auditoria?
       user.has_permission?(:update_external_order_applicant)
     end
