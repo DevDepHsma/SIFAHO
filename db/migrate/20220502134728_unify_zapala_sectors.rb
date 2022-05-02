44f9a35539c3aa376b733dd383fbb926e1f7ca58
paul <paul.18.sma@gmail.com>
2022-05-02 13:14:32 -0300

    fix migration unify sectors#

--- db/migrate/20220502134728_unify_zapala_sectors.rb
+++ db/migrate/20220502134728_unify_zapala_sectors.rb
@@ -1,78 +1,74 @@
 class UnifyZapalaSectors < ActiveRecord::Migration[5.2]
   def change
     @target = Sector.find(386)
-    @covid_19_2 = Sector.find(450)
-    @covid_19_3 = Sector.find(495)
-    @covid_19_pediatria = Sector.find(507)
 
-    OutpatientPrescription.where(provider_sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |op|
+    OutpatientPrescription.where(provider_sector_id: [450, 495, 507]).each do |op|
       op.provider_sector_id = @target.id
       op.save(validate: false)
     end
 
-    ChronicPrescription.where(provider_sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |cp|
+    ChronicPrescription.where(provider_sector_id: [450, 495, 507]).each do |cp|
       cp.provider_sector_id = @target.id
       cp.save(validate: false)
     end
 
-    InternalOrder.where(applicant_sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |cp|
+    InternalOrder.where(applicant_sector_id: [450, 495, 507]).each do |cp|
       cp.applicant_sector_id = @target.id
       cp.save(validate: false)
     end
 
-    InternalOrder.where(provider_sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |cp|
+    InternalOrder.where(provider_sector_id: [450, 495, 507]).each do |cp|
       cp.provider_sector_id = @target.id
       cp.save(validate: false)
     end
 
-    ExternalOrder.where(applicant_sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |cp|
+    ExternalOrder.where(applicant_sector_id: [450, 495, 507]).each do |cp|
       cp.applicant_sector_id = @target.id
       cp.save(validate: false)
     end
 
-    ExternalOrder.where(provider_sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |cp|
+    ExternalOrder.where(provider_sector_id: [450, 495, 507]).each do |cp|
       cp.provider_sector_id = @target.id
       cp.save(validate: false)
     end
 
-    Stock.where(sector_id: [@covid_19_2.id, @covid_19_3.id, @covid_19_pediatria.id]).each do |cp|
+    Stock.where(sector_id: [450, 495, 507]).each do |cp|
       cp.sector_id = @target.id
       cp.save(validate: false)
     end
 
     @target_ucc = Sector.find(394)
-    @ucc_itinerante = Sector.find(509)
-    OutpatientPrescription.where(provider_sector_id: @ucc_itinerante.id).each do |op|
+    OutpatientPrescription.where(provider_sector_id: 509).each do |op|
       op.provider_sector_id = @target_ucc.id
       op.save(validate: false)
     end
 
-    ChronicPrescription.where(provider_sector_id: @ucc_itinerante.id).each do |cp|
+    ChronicPrescription.where(provider_sector_id: 509).each do |cp|
       cp.provider_sector_id = @target_ucc.id
       cp.save(validate: false)
     end
 
-    InternalOrder.where(applicant_sector_id: @ucc_itinerante.id).each do |cp|
+    InternalOrder.where(applicant_sector_id: 509).each do |cp|
       cp.applicant_sector_id = @target_ucc.id
       cp.save(validate: false)
     end
 
-    InternalOrder.where(provider_sector_id: @ucc_itinerante.id).each do |cp|
+    InternalOrder.where(provider_sector_id: 509).each do |cp|
       cp.provider_sector_id = @target_ucc.id
       cp.save(validate: false)
     end
 
-    ExternalOrder.where(applicant_sector_id: @ucc_itinerante.id).each do |cp|
+    ExternalOrder.where(applicant_sector_id: 509).each do |cp|
       cp.applicant_sector_id = @target_ucc.id
       cp.save(validate: false)
     end
 
-    ExternalOrder.where(provider_sector_id: @ucc_itinerante.id).each do |cp|
+    ExternalOrder.where(provider_sector_id: 509).each do |cp|
       cp.provider_sector_id = @target_ucc.id
       cp.save(validate: false)
     end
 
-    Stock.where(sector_id: @ucc_itinerante.id).each do |cp|
+    Stock.where(sector_id: 509).each do |cp|
       cp.sector_id = @target_ucc.id
       cp.save(validate: false)
     end
