0fa8d3c90efbc6a9b24c6e939b2735c38f623bbc
paul <paul.18.sma@gmail.com>
2022-05-05 14:14:20 -0300

    adds establishments rspec

--- spec/features/establishment_spec.rb
+++ spec/features/establishment_spec.rb
@@ -36,13 +36,67 @@ RSpec.feature "Establishments", type: :feature do
         end
 
         within '#establishments' do
-          expect(page).to have_selector('.btn-detail', count: 1)
+          expect(page).to have_selector('.btn-detail', count: 2)
           page.execute_script %Q{$('a.btn-detail')[0].click()}
-          sleep 1
+        end
+        expect(page).to have_content('Viendo establecimiento')
+        expect(page).to have_content('Información')
+        expect(page).to have_content('Ubicación')
+        expect(page).to have_content('Sectores')
+        expect(page).to have_content('Usuarios')
+        expect(page.has_link?('Volver')).to be true
+        click_link 'Volver'
+        PermissionUser.create(user: @user, sector: @user.sector, permission: @create_establishments)
+        visit current_path
+        within '#dropdown-menu-header' do
+          expect(page.has_link?('Agregar')).to be true
+          click_link 'Agregar'
+        end
+
+        expect(page.has_css?('#establishment_name')).to be true
+        expect(page.has_css?('#establishment_short_name')).to be true
+        expect(page.has_css?('#sanitary_zone', visible: false)).to be true
+        expect(page.has_css?('#establishment_type', visible: false)).to be true
+        within '#new_establishment' do
+          fill_in 'establishment_name', with: 'Hospital Villa La Angostura'
+          fill_in 'establishment_short_name', with: 'HVLA'
+          click_button 'Seleccionar zona sanitaria'
+          find('button', text: 'Seleccionar zona sanitaria').sibling('div', class: 'dropdown-menu').find('a', text: 'Zona Sanitaria IV').click
+          click_button 'Seleccionar tipo'
+          find('button', text: 'Seleccionar tipo').sibling('div', class: 'dropdown-menu').find('a', text: 'Hospital').click
+        end
+        click_button 'Guardar'
+        click_link 'Volver'
+        within '#establishments' do
+          expect(page).to have_selector('.btn-detail', count: 3)
+        end
+        PermissionUser.create(user: @user, sector: @user.sector, permission: @update_establishments)
+        visit current_path
+        within '#establishments' do
+          expect(page).to have_selector('.btn-edit', count: 3)
+          page.execute_script %Q{$('a.btn-edit')[0].click()}
+        end
+        expect(page).to  have_content('Editando establecimiento')
+        expect(page.has_link?('Volver')).to be true
+        expect(page.has_button?('Guardar')).to be true
+        click_link 'Volver'
+        PermissionUser.create(user: @user, sector: @user.sector, permission: @destroy_establishments)
+        visit current_path
+        within '#establishments' do
+          expect(page).to have_selector('.delete-item', count: 1)
+          page.execute_script %Q{$('button.delete-item')[0].click()}
+        end
+        sleep 1
+        expect(page).to have_content('Eliminar establecimiento')
+        expect(page.has_button?('Volver')).to be true
+        expect(page.has_link?('Confirmar')).to be true
+        click_link 'Confirmar'
+        sleep 1
+        within '#establishments' do
+          expect(page).to have_selector('.delete-item', count: 0)
+          page.execute_script %Q{$('button.delete-item')[0].click()}
         end
       end
-
-
     end
   end
 end
