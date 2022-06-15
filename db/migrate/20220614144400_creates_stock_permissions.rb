7029a8f6bfc456a105a67c2d8a0842ae472597fc
paul <paul.18.sma@gmail.com>
2022-06-15 10:07:00 -0300

    creates stocks permissions

--- db/migrate/20220614144400_creates_stock_permissions.rb
+++ db/migrate/20220614144400_creates_stock_permissions.rb
@@ -1,4 +1,9 @@
 class CreatesStockPermissions < ActiveRecord::Migration[5.2]
   def change
+    stocks = PermissionModule.create(name: 'Stock')
+    Permission.create(name: 'read_stocks', permission_module: stocks)
+    Permission.create(name: 'read_archive_stocks', permission_module: stocks)
+    Permission.create(name: 'create_archive_stocks', permission_module: stocks)
+    Permission.create(name: 'return_archive_stocks', permission_module: stocks)
   end
 end
