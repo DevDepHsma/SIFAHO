3663a414fe3b68ad719324764195a0aaa67c03eb
paul <paul.18.sma@gmail.com>
2022-08-30 06:44:28 -0300

    upgrade user rspec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -42,7 +42,8 @@ RSpec.feature 'Users', type: :feature, js: true do
           expect(page.has_link?('Solicitud de permisos')).to be true
           click_link 'Solicitud de permisos'
         end
-        create(:permission_req_1, user: @user_requested)
+        role = Role.first
+        create(:permission_request, user: @user_requested, roles: [role])
         visit current_path
         # Answer request
         within '#permission_requests' do
@@ -150,6 +151,10 @@ RSpec.feature 'Users', type: :feature, js: true do
             button.next('.dropdown-menu').find('a.dropdown-item').first().click();
           }
           sleep 1
+          role = get_roles.sample
+          page.execute_script %Q{
+            $('label:contains("#{role}")').first().click()
+          }
           fill_in 'permission_request_observation', with: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
         end
         click_button 'Enviar'
--- test/factories/permission_requests.rb
+++ test/factories/permission_requests.rb
@@ -5,11 +5,5 @@ FactoryBot.define do
     other_establishment { 'Hospital Dr. Ramon Carrillo' }
     sector_id { 0 }
     other_sector { 'Farmacia' }
-
-    trait :req_1 do
-      association :user, factory: :user_6
-    end
-
-    factory :permission_req_1, traits: %i[req_1]
   end
 end
