e239e2ff84989e772d9dc7691b75138038be21fe
paul <paul.18.sma@gmail.com>
2022-09-03 01:03:45 -0300

    upgrade internal applicant template policies, and adds rspec

--- app/policies/internal_order_template_applicant_policy.rb
+++ app/policies/internal_order_template_applicant_policy.rb
@@ -1,13 +1,33 @@
 class InternalOrderTemplateApplicantPolicy < InternalOrderTemplatePolicy
+  def index?
+    show?
+  end
+
+  def show?
+    user.has_permission?(:read_internal_order_applicant)
+  end
+
+  def create?
+    user.has_permission?(:create_internal_order_applicant)
+  end
+
+  def new?
+    create?
+  end
+
+  def update?(resource)
+    edit?(resource)
+  end
+
   def edit?(resource)
     if resource.owner_sector == user.sector && resource.solicitud?
-      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :medic, :enfermero)
+      user.has_permission?(:update_internal_order_applicant)
     end
   end
 
   def use_template?(resource)
     if resource.solicitud? && resource.owner_sector == user.sector
-      user.has_any_role?(:admin, :farmaceutico, :auxiliar_farmacia, :medic, :enfermero)
+      user.has_permission?(:create_internal_order_applicant)
     end
   end
 end
