03ad80fa6899a0274c9daf77c0d6ad6fd2d8b9bd
paul <paul.18.sma@gmail.com>
2022-09-12 15:55:34 -0300

    transform

--- app/controllers/reports/reports_controller.rb
+++ app/controllers/reports/reports_controller.rb
@@ -6,33 +6,44 @@ class ReportsController < ApplicationController
       flash[:error] = 'Usted no está autorizado para realizar esta acción.'
       redirect_back(fallback_location: root_path)
     end
-    
   end
 
   def new
     policy(:report).new?
     @report = Report.new
+    @products = Product.select(:id, :name,
+                               :code).where(id: Stock.where(sector_id: @current_user.sector_id).pluck(:product_id)).limit(20)
+    # Product.select(:id, :name, :code).limit(10)
   end
-  
+
   def create
     policy(:report).create?
-    @report = Report.create(sector: @current_user.sector, 
-                            sector_name: @current_user.sector.name, 
+    @report = Report.create(sector: @current_user.sector,
+                            sector_name: @current_user.sector.name,
                             establishment_name: @current_user.sector.establishment.name,
                             generated_date: Time.now,
                             generated_by_user_id: @current_user.id,
                             report_type: report_params(:report_type))
-    puts "<======================".colorize(background: :red)
-    # @report = Report.new
   end
+
+  def get_stock_products
+    @products = Product.select(:id, :name, :code).where(id: Stock.where(sector_id: @current_user.sector_id).pluck(:product_id)).where(
+      'code like ? OR unaccent(lower(name)) like ?', "%#{params[:term]}%", "%#{params[:term].downcase.parameterize}%"
+    ).limit(20)
+  end
+
+  ###################################  DEPRECATED  ########################################
 
   def show
     authorize @report
 
     if @report.delivered_by_order?
-      @pres_consumption = current_user.sector.sum_delivered_prescription_quantities_to(@report.supply_id, @report.since_date, @report.to_date) 
-      @int_ord_consumption = current_user.sector.sum_delivered_internal_quantities_to(@report.supply_id, @report.since_date, @report.to_date)
-      @ord_sup_consumption = current_user.sector.sum_delivered_external_order_quantities_to(@report.supply_id, @report.since_date, @report.to_date)
+      @pres_consumption = current_user.sector.sum_delivered_prescription_quantities_to(@report.supply_id,
+                                                                                       @report.since_date, @report.to_date)
+      @int_ord_consumption = current_user.sector.sum_delivered_internal_quantities_to(@report.supply_id,
+                                                                                      @report.since_date, @report.to_date)
+      @ord_sup_consumption = current_user.sector.sum_delivered_external_order_quantities_to(@report.supply_id,
+                                                                                            @report.since_date, @report.to_date)
       @total_sum = @pres_consumption + @int_ord_consumption + @ord_sup_consumption
     elsif @report.delivered_by_establishment?
       @quantities = current_user.sector.delivered_external_order_quantities_by_establishment_to(@report.supply_id)
@@ -60,14 +71,14 @@ class ReportsController < ApplicationController
 
     @report.sector = current_user.sector
     @report.user = current_user
-    @report.name = "Reporte de insumo entregado por pedido"
+    @report.name = 'Reporte de insumo entregado por pedido'
 
     respond_to do |format|
       if @report.save
-        flash.now[:success] = @report.name+" se ha creado correctamente."
+        flash.now[:success] = @report.name + ' se ha creado correctamente.'
         format.html { redirect_to @report }
       else
-        flash.now[:error] = "El reporte no se ha podido crear."
+        flash.now[:error] = 'El reporte no se ha podido crear.'
         format.html { render :new_delivered_by_order }
       end
     end
@@ -81,27 +92,28 @@ class ReportsController < ApplicationController
     @report.sector = current_user.sector
     @report.user = current_user
     @report.report_type = 1
-    @report.name = "Reporte de insumo entregado por establecimiento."
+    @report.name = 'Reporte de insumo entregado por establecimiento.'
 
     respond_to do |format|
       if @report.save
-        flash.now[:success] = @report.name+" se ha creado correctamente."
+        flash.now[:success] = @report.name + ' se ha creado correctamente.'
         format.html { redirect_to @report }
       else
-        flash.now[:error] = "El reporte no se ha podido crear."
+        flash.now[:error] = 'El reporte no se ha podido crear.'
         format.html { render :new_delivered_by_establishment }
       end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def report_params
-      params.require(:report).permit(:id, :report_type)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def report_params
+    params.require(:report).permit(:id, :report_type)
+  end
 end
--- config/routes/report_routes.rb
+++ config/routes/report_routes.rb
@@ -54,6 +54,10 @@ Rails.application.routes.draw do
       # end
       # end
     end
-    resources :reports, only: %i[index new create]
+    resources :reports, only: %i[index new create] do
+      collection do
+        get :get_stock_products,  to: 'reports#get_stock_products', as: 'get_stock_products'
+      end
+    end
   end
 end
