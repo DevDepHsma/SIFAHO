50cf8bc9030e7a047e16c0546c9f227716241384
paul <paul.18.sma@gmail.com>
2022-09-12 15:55:34 -0300

    transform

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,3 +1,16 @@
 $(document).on('turbolinks:load', function() {
+  $("#products-search").on('keyup', function(e){
+    const regexp = /^[a-zA-Z | 0-9 | áéíóúñ%°,.-]$/ig;
+    if(e.key.match(regexp) || e.key === 'Backspace'){
+      $.ajax({
+        url: $(e.target).attr('data-url'),
+        contentType: 'script',
+        data: {term: e.target.value}
+      });
+    }
+  });
 
+  $(".link-add-product").on('click', function(e){
+    console.log($(e.target).attr('data-value'));
+  });
 });
