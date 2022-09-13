85dc39ea6a2e4e488a2219f53d1a079984dbe4b7
paul <paul.18.sma@gmail.com>
2022-09-13 11:14:19 -0300

    adds loading icon

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,8 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $("#products-search").on('keyup', function(e){
     const regexp = /^[a-zA-Z | 0-9 | áéíóúñ%°,.-]$/ig;
-    const product_ids = $("input[name='product_ids']").val();
     if(e.key.match(regexp) || e.key === 'Backspace'){
+      $(e.target).attr('disabled', true);
+      $("#loader-container").css({'display': 'flex'});
+      const product_ids = $("input[name='product_ids']").val();
       $.ajax({
         url: $(e.target).attr('data-url'),
         contentType: 'script',
@@ -13,6 +15,7 @@ $(document).on('turbolinks:load', function() {
 });
 
 function toggleSelectProduct(event){
+  $("#loader-container").css({'display': 'flex'});
   const product_ids = $("input[name='product_ids']").val();
   const product_term = $("input#products-search").val();
   $.ajax({
