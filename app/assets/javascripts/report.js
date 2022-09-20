17db47b6493e975c38927788d9f912f05aa3e919
paul <paul.18.sma@gmail.com>
2022-09-19 22:27:27 -0300

    adds delay on ajax search

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,19 +1,25 @@
 $(document).on('turbolinks:load', function() {
+  let $_AJAX
   $("#products-search , #patients-search").on('keyup', function(e){
     const regexp = /^[a-zA-Z | 0-9 | áéíóúñ%°,.-]$/ig;
     if((e.key.match(regexp) || e.key === 'Backspace') && ($(e.target).val().length > 2 || $(e.target).val().length == 0)){
-      $(e.target).attr('disabled', true);
-      $("#loader-container").css({'display': 'flex'});
-      const product_ids = $("input[name='report[product_ids]']").val();
-      const patient_ids = $("input[name='report[patient_ids]']").val();
-      $.ajax({
-        url: $(e.target).attr('data-url'),
-        contentType: 'script',
-        data: {term: e.target.value, product_ids, patient_ids }
-      });
+      clearTimeout($_AJAX);
+      $_AJAX = setTimeout(function(){
+        $(e.target).attr('disabled', true);
+        $("#loader-container").css({'display': 'flex'});
+        const product_ids = $("input[name='report[product_ids]']").val();
+        const patient_ids = $("input[name='report[patient_ids]']").val();
+        $.ajax({
+          url: $(e.target).attr('data-url'),
+          contentType: 'script',
+          data: {term: e.target.value, product_ids, patient_ids }
+        });
+      }, 700);      
     }
   });
 });
+
+
 
 function toggleSelectItem(event, type){
   $("#loader-container").css({'display': 'flex'});
@@ -21,7 +27,7 @@ function toggleSelectItem(event, type){
   const patient_ids = $("input[name='report[patient_ids]']").val();
   const term = type === 'product' ? $("input#products-search").val() : $("input#patients-search").val();
   $.ajax({
-    url: $(event.target).attr('data-url'),
+    url: event.target.getAttribute('data-url'),
     contentType: 'script',
     data: { term, product_ids, patient_ids }
   });
