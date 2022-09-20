6450542f370c8214a5c6c8d4a5a9e0f8b4f0fb0d
paul <paul.18.sma@gmail.com>
2022-09-19 22:07:50 -0300

    adds chronic_prescription_products dispensations to report by patient!

--- app/models/reports/report.rb
+++ app/models/reports/report.rb
@@ -11,26 +11,66 @@ class Report < ApplicationRecord
 
   def set_by_patients(args)
     opproducts = OutpatientPrescriptionProduct.get_delivery_products_by_patient({ sector_id: sector_id,
-                                                                                       patient_ids: args[:patient_ids].split('_'),
-                                                                                       product_ids: args[:product_ids].split('_') })
+                                                                                  patient_ids: args[:patient_ids].split('_'),
+                                                                                  product_ids: args[:product_ids].split('_') })
 
     cpproducts = ChronicPrescriptionProduct.get_delivery_products_by_patient({ sector_id: sector_id,
-                                                                                    patient_ids: args[:patient_ids].split('_'),
-                                                                                    product_ids: args[:product_ids].split('_') })
+                                                                               patient_ids: args[:patient_ids].split('_'),
+                                                                               product_ids: args[:product_ids].split('_') })
 
-    # pproducts = 
-    opproducts.each do |opp|
+    query = "SELECT
+              SUM(t3.product_quantity) as product_quantity,
+              t3.product_id,
+              t3.patient_id,
+              t3.product_code,
+              t3.product_name,
+              t3.patient_full_name,
+              t3.patient_dni,
+              t3.patient_birthdate,
+              t3.patient_birthdate
+            FROM (
+              SELECT
+                t1.product_id,
+                t1.patient_id,
+                t1.product_code,
+                t1.product_name,
+                t1.product_quantity,
+                t1.patient_full_name,
+                t1.patient_dni,
+                t1.patient_birthdate
+              FROM (#{opproducts.to_sql}) as t1
+              UNION
+              SELECT
+                t2.product_id,
+                t2.patient_id,
+                t2.product_code,
+                t2.product_name,
+                t2.product_quantity,
+                t2.patient_full_name,
+                t2.patient_dni,
+                t2.patient_birthdate
+              FROM (#{cpproducts.to_sql}) as t2) as t3
+            GROUP BY t3.patient_id,
+                    t3.product_id,
+                    t3.product_code,
+                    t3.product_name,
+                    t3.patient_full_name,
+                    t3.patient_dni,
+                    t3.patient_birthdate"
+    dispensed_products = ActiveRecord::Base.connection.execute(query).entries
+
+    dispensed_products.each do |dp|
       ReportPatient.create(
         report_id: id,
-        product_id: opp.product_id,
-        patient_id: opp.patient_id,
-        product_code: opp.product_code,
-        product_name: opp.product_name,
-        product_quantity: opp.product_quantity,
-        patient_dni: opp.patient_dni,
-        patient_full_name: opp.patient_full_name,
-        patient_age: ((Time.zone.now - opp.patient_birthdate.to_time) / 1.year.seconds).floor,
-        patient_birthdate: opp.patient_birthdate
+        product_id: dp['product_id'],
+        patient_id: dp['patient_id'],
+        product_code: dp['product_code'],
+        product_name: dp['product_name'],
+        product_quantity: dp['product_quantity'],
+        patient_dni: dp['patient_dni'],
+        patient_full_name: dp['patient_full_name'],
+        patient_age: ((Time.zone.now - dp['patient_birthdate'].to_time) / 1.year.seconds).floor,
+        patient_birthdate: dp['patient_birthdate']
       )
     end
   end
