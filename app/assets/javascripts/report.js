2f266688a5243182090d2ce546afb94d4940dfb6
paul <paul.18.sma@gmail.com>
2022-09-21 15:45:15 -0300

    adds select styles

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -1,5 +1,16 @@
 $(document).on('turbolinks:load', function() {
   let $_AJAX
+  $("#products-search , #patients-search").on('focusin', function(e){
+    $($(e.target).attr('data-target')).collapse('show');
+  });
+
+  $('.collapse').on('show.bs.collapse', function (e) {
+    $("button[data-target='#" + $(e.target).attr('id') + "']").find('svg').first().addClass('fa-rotate-180')
+  });
+  $('.collapse').on('hide.bs.collapse', function (e) {
+    $("button[data-target='#" + $(e.target).attr('id') + "']").find('svg').first().removeClass('fa-rotate-180')
+  });
+
   $("#products-search , #patients-search").on('keyup', function(e){
     const regexp = /^[a-zA-Z | 0-9 | áéíóúñ%°,.-]$/ig;
     if(e.key.match(regexp) || e.key === 'Backspace'){
