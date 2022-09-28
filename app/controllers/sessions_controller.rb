5692268a5dda00bd88ac23b59273d95242aa0991
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-09-28 11:00:54 -0300

    Update authentication for production env

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < Devise::SessionsController
 
   def create
     # Andes server ip
-    if ping('192.168.20.112')
+    if ping('192.168.20.112') && Rails.env.production?
       resource = warden.authenticate!(scope: resource_name, recall: "#{controller_path}#new")
       set_flash_message(:notice, :signed_in) if is_navigational_format?
       resource.password = params[:user][:password]
