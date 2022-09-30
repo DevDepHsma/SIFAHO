dca216426a075848ef9b01d2e29c6ce1f01f1302
paul <paul.18.sma@gmail.com>
2022-09-30 15:29:51 -0300

    adds products selecition rspec on report by patient

--- spec/features/reports/report_by_patient_spec.rb
+++ spec/features/reports/report_by_patient_spec.rb
@@ -23,26 +23,98 @@ RSpec.feature 'Reports::ExternalOrderProductReports', type: :feature do
         visit '/'
       end
 
-      it 'Reports permission' do
-        expect(page.has_css?('#sidebar-wrapper')).to be true
-        within '#sidebar-wrapper' do
-          expect(page.has_link?('Reportes')).to be true
-          click_link 'Reportes'
-        end
-        within '#dropdown-menu-header' do
-          # expect(page.has_link?('Reportes')).to be true
-        end
-
-        # By Patient
-        expect(page).to have_content('Nuevo reporte')
-        expect(page.has_css?('#new_report')).to be true
-        within '#new_report' do
-          expect(page).to have_content('Desde')
-          expect(page).to have_content('Hasta')
-          expect(page).to have_content('Productos')
-          expect(page).to have_content('Pacientes')
+      # it 'Reports permission' do
+      #   expect(page.has_css?('#sidebar-wrapper')).to be true
+      #   within '#sidebar-wrapper' do
+      #     expect(page.has_link?('Reportes')).to be true
+      #     click_link 'Reportes'
+      #   end
+      #   within '#dropdown-menu-header' do
+      #     expect(page.has_link?('Reportes')).to be true
+      #   end
+      # end
+      describe 'Form' do
+        before(:each) do
+          within '#sidebar-wrapper' do
+            click_link 'Reportes'
+          end
         end
 
+        # it 'Report labels / inputs' do
+        #   # By Patient
+        #   expect(page).to have_content('Nuevo reporte')
+        #   expect(page.has_css?('#new_report')).to be true
+        #   within '#new_report' do
+        #     Report.report_types.each do |type|
+        #       expect(page).to have_content(I18n.t("activerecord.attributes.report.report_type.#{type[0]}"))
+        #     end
+        #     expect(page).to have_content('Desde')
+        #     expect(page).to have_content('Hasta')
+        #     expect(page).to have_content('Productos')
+        #     expect(page).to have_content('Pacientes')
+
+        #     expect(page.has_field?('report[from_date]', type: 'text')).to be true
+        #     expect(page.has_field?('report[to_date]', type: 'text')).to be true
+        #     expect(page.has_field?('report[product_ids]', type: 'hidden')).to be true
+        #     expect(page.has_field?('report[patient_ids]', type: 'hidden')).to be true
+        #     expect(page.has_field?('report[report_type]', type: 'radio', visible: false)).to be true
+        #     expect(page.has_field?('report[all_products]', type: 'checkbox', visible: false)).to be true
+        #     expect(page.has_field?('report[all_patients]', type: 'checkbox', visible: false)).to be true
+
+        #     expect(page.has_field?('products-search', type: 'text')).to be true
+        #     expect(page.has_field?('patients-search', type: 'text')).to be true
+        #   end
+        # end
+
+        describe 'Fill report form' do
+          before(:each) do
+            @product = Product.first
+            @patient = Patient.first
+          end
+          it 'Fill products' do
+            within '#new_report' do
+              page.find('input#products-search').click.set(@product.code)
+              sleep 1
+              expect(page.has_css?('#products-collapse')).to be true
+
+              within '#products-collapse' do
+                expect(page.has_button?("#{@product.code} | #{@product.name}")).to be true
+                click_button "#{@product.code} | #{@product.name}"
+              end
+              sleep 1
+              expect(page.has_css?('#selected-products')).to be true
+              within '#selected-products' do
+                expect(page.has_button?("#{@product.code} | #{@product.name}")).to be true
+              end
+            end
+          end
+          
+          it 'Fill patients' do
+            within '#new_report' do
+              page.find('input#patients-search').click.set(@patient.dni)
+              sleep 1
+              expect(page.has_css?('#patients-collapse')).to be true
+sleep 10
+              within '#patients-collapse' do
+                expect(page.has_button?("#{@patient.dni} | #{@patient.last_name.upcase} #{@patient.first_name.upcase}")).to be true
+                click_button "#{@patient.dni} | #{@patient.last_name.upcase} #{@patient.first_name.upcase}"
+              end
+              sleep 1
+              expect(page.has_css?('#selected-patients')).to be true
+              within '#selected-patients' do
+                expect(page.has_button?("#{@patient.dni} | #{@patient.last_name.upcase} #{@patient.first_name.upcase}")).to be true
+              end
+            end
+          end
+
+          it 'Fill report form' do
+            within '#new_report' do
+              page.find('label', text: 'Por paciente').click
+              fill_in 'report[from_date]', with: '01/01/2022'
+              fill_in 'report[to_date]', with: Time.now.strftime('%d/%m/%Y')
+            end
+          end
+        end
       end
     end
   end
