aae05ee982d1f170eedd84f54ddbd3dc208d767e
paul <paul.18.sma@gmail.com>
2022-10-06 06:47:25 -0300

    db reports populate

--- spec/support/db_populate/reports.rb
+++ spec/support/db_populate/reports.rb
@@ -7,5 +7,13 @@ def reports_populate
                       from_date: (DateTime.now - 1.month).strftime('%d/%m/%Y'),
                       to_date: DateTime.now.strftime('%d/%m/%Y') }
     Report.new.generate!(@farm_applicant, report_params)
+
+    report_params = { name: report_name,
+                      report_type: 1,
+                      product_ids: @products_to_dispense.pluck(:id).join('_'),
+                      patient_ids: Patient.all.sample.id.to_s,
+                      from_date: (DateTime.now - 1.month).strftime('%d/%m/%Y'),
+                      to_date: DateTime.now.strftime('%d/%m/%Y') }
+    Report.new.generate!(@depo_applicant, report_params)
   end
 end
