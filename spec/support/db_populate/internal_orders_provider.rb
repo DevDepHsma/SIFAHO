0806400847f470059c57bd92ca5e9a6d493964d4
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-11-10 12:19:20 -0300

    modify populate providers for delete in test

--- spec/support/db_populate/internal_orders_provider.rb
+++ spec/support/db_populate/internal_orders_provider.rb
@@ -4,14 +4,14 @@ def internal_orders_provider_populate
     charset = Array('A'..'Z') + Array('a'..'z')
     remit_code = Array.new(20) { charset.sample }.join
     InternalOrder.create!(provider_sector_id: @farm_applicant.sector_id, applicant_sector_id: @depo_applicant.sector_id,
-                          order_type: 'solicitud', remit_code: remit_code, status: 'solicitud_auditoria', observation: 'esta es una gran observacion', requested_date: DateTime.now)
+                          order_type: 'provision', remit_code: remit_code, status: 'proveedor_auditoria', observation: 'esta es una gran observacion', requested_date: DateTime.now)
   end
   
   10.times do
     charset = Array('A'..'Z') + Array('a'..'z')
     remit_code = Array.new(20) { charset.sample }.join
     InternalOrder.create!(provider_sector_id: @farm_applicant.sector_id, applicant_sector_id: @depo_applicant.sector_id,
-                          order_type: 'provision', remit_code: remit_code, status: 'provision_en_camino', observation: 'esta es una gran observacion', requested_date: DateTime.now)
+                          order_type: 'solicitud', remit_code: remit_code, status: 'provision_en_camino', observation: 'esta es una gran observacion', requested_date: DateTime.now)
   end
   10.times do
     charset = Array('A'..'Z') + Array('a'..'z')
