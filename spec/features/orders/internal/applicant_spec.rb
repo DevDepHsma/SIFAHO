a69df6ccfd1360a4125a3f492a055e4249b85161
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-11-16 10:44:05 -0300

    update applicant CRUD spec

--- spec/features/orders/internal/applicant_spec.rb
+++ spec/features/orders/internal/applicant_spec.rb
@@ -1,195 +1,147 @@
 require 'rails_helper'
 
 RSpec.feature 'Orders::Internal::Applicants', type: :feature do
-  background do
+  before(:all) do
     perimssion_module = PermissionModule.includes(:permissions).find_by(name: 'Ordenes Internas Solicitud')
     @read_internal_order_applicant = perimssion_module.permissions.find_by(name: 'read_internal_order_applicant')
     @create_internal_order_applicant = perimssion_module.permissions.find_by(name: 'create_internal_order_applicant')
     @update_internal_order_applicant = perimssion_module.permissions.find_by(name: 'update_internal_order_applicant')
     @send_internal_order_applicant = perimssion_module.permissions.find_by(name: 'send_internal_order_applicant')
     @return_internal_order_applicant = perimssion_module.permissions.find_by(name: 'return_internal_order_applicant')
-    @destroy_internal_order_applicant = perimssion_module.permissions.find_by(name: 'destroy_internal_order_applicant')
-    sign_in @farm_applicant
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
+                          permission: @create_internal_order_applicant)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
+                          permission: @read_internal_order_applicant)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
+                          permission: @update_internal_order_applicant)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
+                          permission: @send_internal_order_applicant)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
+                          permission: @return_internal_order_applicant)
   end
 
-  describe 'Permissions', js: true do
+  background do
+    sign_in @farm_applicant
+  end
+  describe '', js: true do
     subject { page }
 
-    it ':: READ :: fail' do
-      expect(page.has_link?('Sectores')).to_not be true
+    describe 'Permission:' do
+      before(:each) do
+        visit '/sectores'
+      end
+      it 'list' do
+        expect(page).to have_css('#table_results')
+      end
+
+      it 'show' do
+        click_link 'Recibos'
+        order = InternalOrder.where(status: 'solicitud_auditoria', applicant_sector_id: @farm_applicant.sector_id).first
+        visit "/sectores/pedidos/recibos/#{order.id}"
+        expect(page).to have_content('Solicitante')
+        expect(page).to have_content('Proveedor')
+        expect(page).to have_content('Solicitado')
+        expect(page).to have_content('Código')
+        expect(page).to have_content('Observaciones')
+        expect(page).to have_link('Volver')
+        expect(page).to have_link('Editar orden')
+        expect(page).to have_link('Editar productos')
+        expect(page).to have_link('Enviar solicitud')
+      end
+
+      it 'create' do
+        within '#dropdown-menu-header' do
+          expect(page).to have_link('Solicitar')
+          click_link 'Solicitar'
+        end
+        expect(page).to have_content('Nueva solicitud de sector')
+        expect(page).to have_content('Últimas solicitudes de sectores')
+        expect(page).to have_select('internal_order[provider_sector_id]', visible: false)
+        expect(page).to have_field('internal_order[observation]', type: 'textarea', visible: false)
+        expect(page).to have_link('Volver')
+        expect(page).to have_button('Guardar y agregar productos')
+      end
+
+      it 'edit' do
+        within '#dropdown-menu-header' do
+          expect(page).to have_link('Solicitar')
+          click_link 'Solicitar'
+        end
+        within '#order-form' do
+          select_sector(@depo_applicant.sector.name, 'select#provider-sector')
+        end
+        expect(page).to have_content(@depo_applicant.sector.name)
+        click_button 'Guardar y agregar productos'
+        expect(page).to have_content('Editando productos de solicitud de sector código')
+        expect(page).to have_content('Solicitante')
+        expect(page).to have_content(@farm_applicant.sector.name)
+        expect(page).to have_content('Proveedor')
+        expect(page).to have_content(@depo_applicant.sector.name)
+        expect(page).to have_content('Código')
+        expect(page).to have_content('Producto')
+        expect(page).to have_content('Unidad')
+        expect(page).to have_content('Tu stock')
+        expect(page).to have_content('Solicitar')
+        expect(page).to have_content('Tu observación')
+        expect(page).to have_link('Volver')
+        expect(page).to have_button('Enviar')
+      end
     end
 
-    describe '' do
+    describe 'Form' do
       before(:each) do
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
-                              permission: @read_internal_order_applicant)
-        visit '/'
+        visit 'sectores/pedidos/recibos'
+      end
+      it 'create' do
+        click_link 'Solicitar'
+        within '#order-form' do
+          select_sector(@depo_est_1.name, 'select#provider-sector')
+        end
+        fill_in 'internal_order[observation]', with: 'Lorem ipsum'
+        click_button 'Guardar y agregar productos'
+        expect(page).to have_content('La solicitud se ha creado y se encuentra en auditoria.')
+      end
+      it 'charge products' do
+        click_link 'Solicitar'
+        within '#order-form' do
+          select_sector(@depo_est_1.name, 'select#provider-sector')
+        end
+        fill_in 'internal_order[observation]', with: 'Lorem ipsum'
+        click_button 'Guardar y agregar productos'
+        add_products(rand(1..3), request_quantity: true, observations: true)
+        click_button 'Enviar'
+        expect(page).to have_content('La solicitud se ha enviado correctamente')
       end
 
-      it ':: READ :: success' do
-        expect(page.has_link?('Sectores')).to be true
-      end
-
-      describe '' do
-        before(:each) do
-          within '#sidebar-wrapper' do
-            click_link 'Sectores'
-          end
+      it 'edit' do
+        within '#internal_orders' do
+          expect(page).to have_selector('tr')
+          expect(page).to have_selector('.btn-edit')
+          page.find('.btn-edit',match: :first).click
         end
-
-        it 'has header links' do
-          expect(page.has_link?('Recibos')).to be true
-          expect(page.has_link?('Entregas')).not_to be true
-          expect(page.has_link?('Solicitar')).not_to be true
-          expect(page.has_link?('Entregar')).not_to be true
-          expect(page.has_link?('Plantillas')).to be true
+        fill_in 'internal_order_observation', with: 'observación editada'
+        click_button 'Guardar y agregar productos'
+        expect(page).to have_content('La solicitud se ha modificado correctamente')
+        add_products(rand(1..3), request_quantity: true, observations: true)
+        click_button 'Enviar'
+        expect(page).to have_content('La solicitud se ha enviado correctamente')
+        click_button 'Retornar'
+        expect(page).to have_content('Retornar a solicitud auditoria')
+        expect(page).to have_content('Seguro que desea retornar la solicitud actual a solicitud auditoria?')
+        expect(page).to have_content('De esta manera podrá seguir agregando o quitando productos del pedido.')
+        click_link 'Confirmar'
+        click_link 'Editar productos'
+        page.all('.btn-delete-confirm').each do |product|
+          product.click
+          expect(page).to have_content('Eliminar producto')
+          sleep 1
+          click_link 'Confirmar'
+          sleep 1
         end
-
-        describe ':: CREATE' do
-          it ':: fail' do
-            visit '/sectores/pedidos/recibos/nuevo'
-            expect(page).to have_content('Usted no está autorizado para realizar esta acción.')
-          end
-
-          describe '' do
-            before(:each) do
-              PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
-                                    permission: @create_internal_order_applicant)
-            end
-
-            it ':: visit create form' do
-              expect(page.has_link?('Solicitar')).to be true
-              click_link 'Solicitar'
-              expect(page).to have_content('Nueva solicitud de sector')
-              expect(page).to have_content('Últimas solicitudes de sectores')
-              expect(page.has_css?('select#provider-sector', visible: false)).to be true
-              expect(page.has_css?('textarea#internal_order_observation', visible: false)).to be true
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_button?('Guardar y agregar productos')).to be true
-
-              within '#order-form' do
-                select_sector(@depo_applicant.sector.name, 'select#provider-sector')
-              end
-              expect(page).to have_content(@depo_applicant.sector.name)
-              click_button 'Guardar y agregar productos'
-              expect(page).to have_content('Editando productos de solicitud de sector código')
-              expect(page).to have_content('Solicitante')
-              expect(page).to have_content(@farm_applicant.sector.name)
-              expect(page).to have_content('Proveedor')
-              expect(page).to have_content(@depo_applicant.sector.name)
-              expect(page).to have_content('Código')
-              expect(page).to have_content('Producto')
-              expect(page).to have_content('Unidad')
-              expect(page).to have_content('Tu stock')
-              expect(page).to have_content('Solicitar')
-              expect(page).to have_content('Tu observación')
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_button?('Enviar')).to be false
-
-              PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
-                                    permission: @update_internal_order_applicant)
-              visit current_path
-              add_products(rand(1..3), request_quantity: true, observations: true)
-              expect(page).to have_selector('input.product-code')
-              click_link 'Volver'
-              within '#applicant_orders' do
-                expect(page).to have_selector('tr')
-                expect(page).to have_selector('.btn-edit')
-                page.execute_script %{$('a.btn-edit')[0].click()}
-              end
-
-              expect(page).to have_content('Editando solicitud de sector código')
-              expect(page).to have_selector('#provider-sector', visible: false)
-              expect(page).to have_selector('textarea#internal_order_observation')
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_link?('Editar productos')).to be true
-              expect(page.has_button?('Guardar y agregar productos')).to be true
-              click_button 'Guardar y agregar productos'
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_button?('Enviar')).to be false
-
-              # Add send permission
-              PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
-                                    permission: @send_internal_order_applicant)
-              visit current_path
-              expect(page.has_button?('Enviar')).to be true
-              click_button 'Enviar'
-              expect(page).to have_content('La solicitud se ha enviado correctamente.')
-              expect(page).to have_content('Solicitud enviada')
-              expect(page).to have_content('Productos')
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_link?('Imprimir')).to be true
-              expect(page.has_button?('Retornar')).to be false
-              # Add return permission
-              PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
-                                    permission: @return_internal_order_applicant)
-              visit current_path
-              expect(page.has_button?('Retornar')).to be true
-              click_button 'Retornar'
-              sleep 1
-              expect(page).to have_content('Retornar a solicitud auditoria')
-              expect(page.has_link?('Cancelar')).to be true
-              expect(page.has_link?('Confirmar')).to be true
-              click_link 'Confirmar'
-              expect(page).to have_content('Solicitud auditoria')
-              click_link 'Volver'
-              within '#applicant_orders' do
-                expect(page).not_to have_selector('.delete-item')
-              end
-              # Add destroy permission
-              PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.sector,
-                                    permission: @destroy_internal_order_applicant)
-              visit current_path
-              within '#applicant_orders' do
-                expect(page).to have_selector('.delete-item')
-                page.execute_script %{$('button.delete-item')[0].click()}
-                sleep 1
-              end
-              expect(page).to have_content('Eliminar solicitud')
-              expect(page.has_button?('Volver')).to be true
-              expect(page.has_link?('Confirmar')).to be true
-              click_link 'Confirmar'
-              sleep 1
-            end
-
-            it 'Templates' do
-              within '#dropdown-menu-header' do
-                expect(page.has_link?('Plantillas')).to be true
-                click_link 'Plantillas'
-              end
-              expect(page).to have_content('Plantillas de solicitud')
-              expect(page).not_to have_content('Plantillas de despacho')
-              expect(page.has_css?('#btn-applicant-template')).to be true
-              find('#btn-applicant-template').click
-              expect(page).to have_content('Agregar plantilla de solicitud a sector')
-              expect(page.has_css?('#new_internal_order_template')).to be true
-
-              fill_order_template(
-                form_id: '#new_internal_order_template',
-                template_name_input: 'internal_order_template_name',
-                template_name: 'Template Test',
-                sector_input: 'provider-sector',
-                sector: @depo_applicant.sector,
-                products_size: 3
-              )
-
-              expect(page.has_button?('Guardar')).to be true
-              click_button 'Guardar'
-              expect(page).to have_content('Viendo plantilla de solicitud')
-              expect(page.has_css?('.delete-item')).to be true
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_link?('Imprimir')).to be true
-              expect(page.has_link?('Editar')).to be true
-              expect(page.has_link?('Crear solicitud')).to be true
-              click_link 'Editar'
-              expect(page).to have_content('Editando plantilla de solicitud a sector')
-              expect(page.has_link?('Volver')).to be true
-              expect(page.has_button?('Guardar')).to be true
-              click_link 'Volver'
-              expect(page).to have_content('Template Test')
-            end
-          end
-        end
+        click_link 'Agregar producto'
+        sleep 1
+        add_products(rand(1..3), request_quantity: true, observations: true)
+        click_button 'Enviar'
       end
     end
   end
