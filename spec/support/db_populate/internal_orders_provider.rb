c0c25b0ecb42f97d0968963abf856c68f28856c1
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-11-16 15:46:09 -0300

    update populate for internal orders providers

--- spec/support/db_populate/internal_orders_provider.rb
+++ spec/support/db_populate/internal_orders_provider.rb
@@ -1,9 +1,16 @@
 def internal_orders_provider_populate
-  10.times do
+  5.times do
     charset = Array('A'..'Z') + Array('a'..'z')
     remit_code = Array.new(20) { charset.sample }.join
     order=InternalOrder.new(provider_sector_id: @farm_applicant.sector_id, applicant_sector_id: @depo_applicant.sector_id,
                           order_type: 'provision', remit_code: remit_code, status: 'proveedor_auditoria', observation: 'esta es una gran observacion', requested_date: DateTime.now)
+                          order.save(validate:false)
+  end
+  5.times do
+    charset = Array('A'..'Z') + Array('a'..'z')
+    remit_code = Array.new(20) { charset.sample }.join
+    order=InternalOrder.new(provider_sector_id: @farm_applicant.sector_id, applicant_sector_id: @depo_applicant.sector_id,
+                          order_type: 'solicitud', remit_code: remit_code, status: 'proveedor_auditoria', observation: 'esta es una gran observacion', requested_date: DateTime.now)
                           order.save(validate:false)
   end
 
