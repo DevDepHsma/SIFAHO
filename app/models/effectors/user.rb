5ac562981195fc3ac9104833c0674ad7caae4f56
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-11-22 12:06:13 -0300

    remove scope deprecated sorted_by

--- app/models/effectors/user.rb
+++ app/models/effectors/user.rb
@@ -150,18 +150,7 @@ class User < ApplicationRecord
                   using: { tsearch: { prefix: true } }, # Buscar coincidencia desde las primeras letras.
                   ignoring: :accents # Ignorar tildes.
 
-  scope :sorted_by, lambda { |sort_option|
-    # extract the sort direction from the param value.
-    direction = sort_option =~ /desc$/ ? 'desc' : 'asc'
-    case sort_option.to_s
-    when /^created_at_/s
-      # Ordenamiento por fecha de creación en la BD
-      order("users.created_at #{direction}")
-    else
-      # Si no existe la opcion de ordenamiento se levanta la excepcion
-      raise(ArgumentError, "Invalid sort option: #{sort_option.inspect}")
-    end
-  }
+  
 
   scope :with_sector_id, lambda { |an_id|
     where(sector_id: [*an_id])
