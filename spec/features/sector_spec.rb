a47232a0d1261d6f0f4980e5dccc9a1181f6c116
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-12-01 12:56:39 -0300

    update sector_spec

--- spec/features/sector_spec.rb
+++ spec/features/sector_spec.rb
@@ -11,6 +11,17 @@ RSpec.feature 'Sectors', type: :feature do
     @destroy_sectors = permission_module.permissions.find_by(name: 'destroy_sectors')
     @select_establishment = permission_module.permissions.find_by(name: 'create_to_other_establishment')
     @read_other_establishments = permission_module.permissions.find_by(name: 'read_other_establishments')
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector, permission: @read_sectors)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector,
+                          permission: @create_sectors)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector,
+                          permission: @select_establishment)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector,
+                          permission: @update_sectors)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector,
+                          permission: @read_other_establishments)
+    PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector,
+                          permission: @destroy_sectors)
   end
 
   background do
@@ -19,81 +30,109 @@ RSpec.feature 'Sectors', type: :feature do
   describe '', js: true do
     subject { page }
 
-    it 'Nav Menu link' do
-      expect(page.has_css?('#sidebar-wrapper')).to be false
+    before(:each) do
+      visit '/'
     end
 
-    describe 'Add permission:' do
-      before(:each) do
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector, permission: @read_sectors)
-        visit '/'
-      end
-
+    describe 'access permissions' do
       it 'Nav Menu link' do
-        expect(page.has_css?('#sidebar-wrapper', visible: false)).to be true
+        expect(page).to have_selector('#sidebar-wrapper', visible: false)
         within '#sidebar-wrapper' do
-          expect(page.has_link?('Sectores')).to be true
+          expect(page).to have_link('Sectores')
           click_link 'Sectores'
         end
         within '#dropdown-menu-header' do
-          expect(page.has_link?('Sectores')).to be true
+          expect(page).to have_link('Sectores')
+        end
+      end
+      
+      describe 'actions' do
+        before(:each) do
+          visit '/sectores'
+        end
+        it 'create' do
+          within '#dropdown-menu-header' do
+            expect(page).to have_link('Crear sector')
+            click_link 'Crear sector'
+          end
+          expect(page).to have_selector('#sector_name')
+          expect(page).to have_selector('#sector_description')
+          expect(page).to have_content(@farm_applicant.active_sector.establishment.name)
+          expect(page).to have_selector('#sector_establishment_id', visible: false)
+        end
+        it 'list' do
+          expect(page).to have_selector('#sectors')
+          within '#sectors' do
+            expect(page).to have_selector('tr')
+            expect(page).to have_selector('.btn-detail')
+            expect(page).to have_selector('.btn-edit')
+            expect(page).to have_selector('.delete-item')
+          end
+        end
+        it 'show' do
+          sector = Sector.all.sample
+          expect(page).to have_selector('#sectors')
+          visit "/sectores/#{sector.id}"
+          expect(page).to have_content("Viendo sector #{sector.name}")
+          expect(page).to have_content("Establecimiento: #{sector.establishment.name}")
+          expect(page).to have_content("Descripción: #{sector.description}")
+          if sector.provide_hospitalization
+            expect(page).to have_content('Internación: Si')
+          else
+            expect(page).to have_content('Internación: No')
+          end
         end
 
+        it 'edit' do
+          within '#sectors' do
+            page.all('a.btn-edit').first.click
+          end
+          expect(page).to have_content('Editando sector')
+          expect(page).to have_link('Volver')
+          expect(page).to have_button('Guardar')
+        end
+      end
+    end
+
+    describe 'actions' do
+      before(:each) do
+        visit '/sectores'
+      end
+      it 'edit' do
         within '#sectors' do
-          expect(page).to have_selector('.btn-detail')
-          page.execute_script %{$('a.btn-detail')[0].click()}
+          page.all('a.btn-edit').first.click
         end
-        expect(page).to have_content('Viendo sector')
-        expect(page).to have_content('Establecimiento')
-        expect(page).to have_content('Descripción')
-        expect(page).to have_content('Usuarios')
-        expect(page.has_link?('Volver')).to be true
-        click_link 'Volver'
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector, permission: @create_sectors)
-        visit current_path
-        within '#dropdown-menu-header' do
-          expect(page.has_link?('Crear sector')).to be true
-          click_link 'Crear sector'
-        end
-        expect(page.has_css?('#sector_name')).to be true
-        expect(page.has_css?('#sector_description')).to be true
-        expect(page).to have_content(@farm_applicant.active_sector.establishment.name)
-        expect(page.has_css?('#sector_establishment_id')).to be false
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector, permission: @select_establishment)
-        visit current_path
-        expect(page.has_css?('#sector_establishment_id', visible: false)).to be true
+        fill_in 'Nombre',	with: 'sometext'
+        fill_in 'Descripción',	with: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!'
+        click_button 'Guardar'
+        sleep 1
+        expect(page).to have_content('Viendo sector sometext')
+      end
+
+      it 'create' do
+       click_link 'Crear sector'
         within '#new_sector' do
-          fill_in 'sector_name', with: 'Cocina'
+          fill_in 'Nombre', with: 'Cocina'
+          fill_in 'Descripción', with: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!'
         end
         click_button 'Guardar'
-        click_link 'Volver'
         sleep 1
-        within '#sectors' do
-          expect(page).to have_selector('.btn-detail')
-        end
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector, permission: @update_sectors)
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector,
-                              permission: @read_other_establishments)
-        visit current_path
-        within '#sectors' do
-          expect(page).to have_selector('.btn-edit')
-          page.execute_script %{$('a.btn-edit')[0].click()}
-        end
-        expect(page).to have_content('Editando sector')
-        expect(page.has_link?('Volver')).to be true
-        expect(page.has_button?('Guardar')).to be true
-        click_link 'Volver'
-        PermissionUser.create(user: @farm_applicant, sector: @farm_applicant.active_sector, permission: @destroy_sectors)
-        visit current_path
+        expect(page).to have_content('Viendo sector Cocina')
+      end
+
+      it 'delete' do
         within '#sectors' do
           expect(page).to have_selector('.delete-item')
-          page.execute_script %{$('td:contains("Cocina")').closest('tr').find('button.delete-item').click()}
+          page.all('tr', text: 'Cocina').first.find('button.delete-item').click
         end
         sleep 1
         expect(page).to have_content('Eliminar sector')
-        expect(page.has_button?('Volver')).to be true
-        expect(page.has_link?('Confirmar')).to be true
+        expect(page).to have_button('Volver')
+        expect(page).to have_link('Confirmar')
+        expect(page).to have_content('Esta seguro que desea eliminar el sector Cocina?')
         click_link 'Confirmar'
+        sleep 1
+        expect(page).to have_content('El sector Cocina se ha eliminado correctamente.')
       end
     end
   end
