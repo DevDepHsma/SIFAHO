f24e87ef158b163950322874fe4fa62caa4aecf2
Santiago Navarro <santiagonavarro@outlook.com.ar>
2022-12-06 09:10:15 -0300

    update filters, pagination and sort of establishments

--- app/controllers/establishments/establishments_controller.rb
+++ app/controllers/establishments/establishments_controller.rb
@@ -8,22 +8,16 @@ class EstablishmentsController < ApplicationController
       flash[:error] = 'Usted no está autorizado para realizar esta acción.'
       redirect_back(fallback_location: root_path)
     end
-    @filterrific = initialize_filterrific(
-      Establishment.select(:id, :cuie, :name, :establishment_type_id, 'SUM(sectors.user_sectors_count) AS total_users')
-      .left_outer_joins(:sectors)
-      .group(:id, :cuie, :name, :establishment_type_id),
-      params[:filterrific],
-      select_options: {
-        sorted_by: Establishment.options_for_sorted_by
-      },
-      persistence_id: false
-    ) or return
-    @establishments = (request.format.xlsx? || request.format.pdf?) ? @filterrific.find : @filterrific.find.page(params[:page]).per(15)
+    @establishments = Establishment.filter_by_params(params[:filter])
+                                   .paginate(page: params[:page], per_page: params[:per_page] || 15)
     respond_to do |format|
       if policy(:establishment).index?
         format.html
         format.js
-        format.xlsx { headers['Content-Disposition'] = "attachment; filename=\"Establecimientos_#{DateTime.now.strftime('%d-%m-%Y')}.xlsx\"" }
+        format.xlsx do
+          headers['Content-Disposition'] =
+            "attachment; filename=\"Establecimientos_#{DateTime.now.strftime('%d-%m-%Y')}.xlsx\""
+        end
       elsif policy(:external_order_applicant).index?
         format.html { redirect_to external_orders_applicants_path }
       elsif policy(:external_order_provider).index?
