3de04bfa237407d705d3a49255780f83b5e19dec
Santiago Navarro <santiagonavarro@outlook.com.ar>
2023-01-03 11:11:41 -0300

    update views permissions add report type

--- app/policies/reports/report_policy.rb
+++ app/policies/reports/report_policy.rb
@@ -8,14 +8,18 @@ class ReportPolicy < ApplicationPolicy
   end
 
   def new?
-    user.has_permission?(:report_by_patients)
+    user.has_permission?(:report_by_patients) || user.has_permission?(:report_by_sectors) || user.has_permission?(:report_by_establishments)
   end
 
   def create?
-    user.has_permission?(:report_by_patients)
+    user.has_permission?(:report_by_patients) || user.has_permission?(:report_by_sectors) || user.has_permission?(:report_by_establishments)
   end
 
   def destroy?
     user.has_permission?(:destroy_reports)
+  end
+
+  def report_by?(type)
+    user.has_permission?(type)
   end
 end
